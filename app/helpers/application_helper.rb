70b417c2ea4ed1a7b62f277ef6cecf7a06a8cd54
TahaBikanerwala <tahabikanerwala@gmail.com>
2025-01-01 19:45:56 +0530

    Added helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # returns title per page basis
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Sample App"
+    if page_title.empty?
+      base_title
+    else
+      # this creates issues with escape characters
+      # "#{page_title} | #{base_title}"
+      page_title + "|" + base_title
+    end
+  end
 end
